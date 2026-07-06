0b36689865363be31f4773c579740d7819ac8f22
Antonio <demarcoa28@gmail.com>
2026-07-06 19:34:17 +0200

    login servlet aggiornata

---
+++ src/main/java/control/LoginServlet.java
@@ -0,0 +1,58 @@
+package control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import dao.UtenteDao;
+import dao.UtenteDaoImpl;
+import model.UtenteBean;
+import utils.SecurityUtils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String passwordInserita = request.getParameter("password");
+
+        DataSource connessioneDB = (DataSource) getServletContext().getAttribute("DataSource");
+        UtenteDao utenteDao = new UtenteDaoImpl(connessioneDB);
+
+        try {
+        	//CERCO EMAIL
+            UtenteBean utente = utenteDao.doRetrieveByEmail(email);
+
+            // cifro la password
+            String passwordCifrata = SecurityUtils.toDigest(passwordInserita);
+
+            if (utente != null && utente.getPassword().equals(passwordCifrata)) {
+
+                HttpSession sessione = request.getSession();
+                sessione.setAttribute("utenteLoggato", utente);
+                sessione.setAttribute("ruolo", utente.getRuolo());
+
+                // Reindirizzo 
+                response.sendRedirect("index.html");
+
+            } else {
+                response.sendRedirect("login.html?errore=credenzialiErrate");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendRedirect("login.html?errore=database");
+        }
+    }
+}
